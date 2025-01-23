1e8e602863f679ee8066f3c5e1d026ca2cec1b1a
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2025-01-23 11:58:01 +0400

    addiing walking zombi.

--- AugmentedReality/ContentView.swift
+++ AugmentedReality/ContentView.swift
@@ -8,33 +8,85 @@
 import SwiftUI
 import RealityKit
 
-struct ContentView : View {
-
-    var body: some View {
-        RealityView { content in
-
-            // Create a cube model
-            let model = Entity()
-            let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-            let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-            model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-            model.position = [0, 0.05, 0]
-
-            // Create horizontal plane anchor for the content
-            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-            anchor.addChild(model)
-
-            // Add the horizontal plane anchor to the scene
-            content.add(anchor)
-
-            content.camera = .spatialTracking
-
+struct ContentView: View {
+  
+  @State private var modelEntity: Entity?
+  @State private var anchor: AnchorEntity?
+  
+  var body: some View {
+    VStack {
+      RealityView { content in
+        content.camera = .spatialTracking
+        if anchor == nil {
+          loadModel(into: content)
         }
-        .edgesIgnoringSafeArea(.all)
+      }
+      .edgesIgnoringSafeArea(.all)
+      HStack {
+        Button("Start") {
+          modelEntity?.stopAllAnimations()
+          playAnimation(named: "global scene animation")
+        }
+        .padding()
+        
+        Button("Stop") {
+          modelEntity?.availableAnimations.forEach({ anm in
+            print(anm.name)
+          })
+          modelEntity?.stopAllAnimations()
+        }
+        .padding()
+      }
     }
-
+  }
+  
+  func collectAnimations(from entity: Entity) -> [AnimationResource] {
+    var animations = entity.availableAnimations
+    for child in entity.children {
+      animations.append(contentsOf: collectAnimations(from: child))
+    }
+    return animations
+  }
+  
+  
+  func loadModel(into content: RealityViewCameraContent)  {
+    do {
+      let loadedEntity = try Entity.load(named: "Woman.usdz")
+      modelEntity = loadedEntity
+      
+      let newAnchor = AnchorEntity(world: .zero)
+      loadedEntity.scale = [0.01,0.01,0.01]
+      loadedEntity.position = [0,-1,-12]
+      newAnchor.addChild(loadedEntity)
+      anchor = newAnchor
+      
+      content.add(newAnchor)
+    } catch {
+      print("Error loading USDZ model: \(error)")
+    }
+  }
+  
+  func playAnimation(named animationName: String) {
+    guard let modelEntity else {
+      print("Model entity is not loaded.")
+      return
+    }
+    if let animation = modelEntity.availableAnimations.first(where: { $0.name == animationName }) {
+      modelEntity.playAnimation(animation.repeat(), transitionDuration: 0.3)
+      
+      let targetPosition = SIMD3<Float>(0, -1, -2)
+      modelEntity.move(to: Transform(scale: modelEntity.transform.scale,
+                                     rotation: modelEntity.transform.rotation,
+                                     translation: targetPosition),
+                       relativeTo: modelEntity.parent,
+                       duration: 15.0)
+    } else {
+      print("Animation '\(animationName)' not found.")
+    }
+  }
+  
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
